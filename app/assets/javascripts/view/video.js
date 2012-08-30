a86d8b6c3bebd22245d30566bf925dffab4e5e23
rswisteria <wisteria.r.s@gmail.com>
2012-08-30 12:41:55 +0900

    Flashの読み込みオプションにwmode='transparent'を付与し、OperaでもFlashの上に要素を描画できるようにした

--- app/assets/javascripts/view/video.js
+++ app/assets/javascripts/view/video.js
@@ -13,10 +13,10 @@ var VideoView = Backbone.View.extend({
 
   setVideo: function (model, value) {
     this.video_id = value;
-    var params = { allowScriptAccess: "always" };
+    var params = { allowScriptAccess: "always", wmode: "transparent" };
     var atts = { id: "player_" + value };
     var url = "http://www.youtube.com/v/" + value + "?enablejsapi=1&playerapiid=" + this.playerApiId;
-    var result = swfobject.embedSWF(url, this.id, "800", "500", "8", null, null, params, atts);
+    var result = swfobject.embedSWF(url, this.id, "800", "520", "8", null, null, params, atts);
   },
 
   ready: function() {
